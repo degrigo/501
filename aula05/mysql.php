6755a7db00a4ef5272087ff3da965645a4b8e1bf
Tadeu Grigolon <degrigo@gmail.com>
2018-03-16 22:32:38 -0300

    Working with PDO transaction

--- aula05/mysql.php
+++ aula05/mysql.php
@@ -2,6 +2,20 @@
 
 echo "<pre>";
 
+// Inserindo dados na tabela via PDO
+
 $conexao = new PDO('mysql:host=localhost;dbname=aula05','tadeu','@da4linux');
 
-var_dump($conexao);
+// $sql = "INSERT INTO tb_alunos(nome, email, senha, cpf, rg, dtNasc) VALUES ('Tadeu Grigolon', 'tadeu@webjump.com.br', '123', '32786521858', '442896785', '1984-05-05')";
+
+$sql = "SELECT * FROM tb_alunos";
+
+$result = $conexao->query($sql);
+
+$alunos = $result->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($alunos);
+
+// $conexao->exec($sql);
+
+// var_dump($conexao);
--- aula05/postgres.php
+++ aula05/postgres.php
@@ -4,4 +4,53 @@ echo "<pre>";
 
 $conexao = new PDO('pgsql:host=localhost;dbname=aula05','tadeu','123');
 
-var_dump($conexao);
+// var_dump($conexao);
+
+// inserindo dados no banco
+// $sql = "INSERT INTO tb_alunos(nome, email, senha, cpf, rg, dtNasc) VALUES ('Tadeu Grigolon', 'tadeu@webjump.com.br', '123', '32786521858', '442896785', '1984-05-05')";
+// ----
+// consultando dados no banco
+// $sql = "SELECT * FROM tb_alunos";
+
+// $result = $conexao->query($sql);
+
+// $alunos = $result->fetchAll(PDO::FETCH_ASSOC);
+
+// print_r($alunos);
+
+// 
+
+// $conexao->exec($sql);
+
+// ----TRANSACTION
+// usa try catch
+// se cair na exceção, printo o erro
+
+// Setattribute = setando atributos para ATTR_ERRMODE e ERRMODE_EXCEPTION
+
+// beginTransaction = executa a query
+
+// commit = salva a ação do sql
+
+// PDOException Tipo da variácel $e setado como objeto
+
+// rollback = não salva as informações no banco
+
+$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+	$conexao->beginTransaction();
+
+	$sql = "INSERT INTO tb_alunos(nome, email, senha, cpf, rg, dtNasc) VALUES ('Tadeu Grigolon', 'tadeu@webjump.com.br', '123', '32786521858', '442896785', '1984-05-05')";
+
+	$conexao->exec($sql);
+
+	$sql = "INSERT INTO tb_alunos(nome, email, senha, cpf, rg, dtNasc) VALUES ('Tadeu Grigolon', 'tadeu@webjump.com.br', '123', '32786521858', '442896785',)";
+
+	$conexao->exec($sql);
+
+	$conexao->commit();
+} catch (PDOException $e) {
+	$conexao->rollback();
+	die("Erro: " . $e->getMessage());
+}
